8507b4b849dc66fcde10847a42800a889e85d40f
calebwolfe <caleb-wolfe@gmx.com>
2025-11-17 09:43:50 -0700

    Global styles and card styles.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { ProductModule } from './product/product.module';
 import { HttpClientModule } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -16,6 +18,8 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
     AppRoutingModule,
     ProductModule,
     HttpClientModule,
+    MatToolbarModule,
+    MatButtonModule,
   ],
   providers: [
     provideAnimationsAsync()
